5f5ed8533039df5d5602162124abafcf3b5801cc
Claude H <hirschclaude@hotmail.fr>
2021-04-29 14:27:54 +0200

    Server express + socketIo

---
+++ src/server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const ServerIo = require("socket.io");
+const io = new ServerIo(server);
+const port = 3000
+
+app.get('/' ,(req,res) =>{
+    res.sendFile(__dirname + '/index.html');
+    // res.send('Hello World!');
+});
+
+io.on('connection', (socket) => {
+    console.log('new user');
+});
+
+server.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+});
