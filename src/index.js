c937236cbddcd515c1f739cfc8316db5c8ea96a6
RICHARD-Quentin <56677909+RICHARD-Quentin@users.noreply.github.com>
2021-04-30 14:06:54 +0200

    add templates

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-console.log('Nothing implemented yet');
+let app = require('express')()
+const exphbs = require('express-handlebars');
+
+app.engine('hbs', exphbs({
+  defaultLayout: 'main',
+  extname: '.hbs'
+}));
+app.set('view engine', 'hbs');
+app.get('/', (request, response) => {
+  response.render('index');
+})
+
+app.listen(8080)
