6bc5160b3f3c423df18eeea73f5ac4eeda2ec979
Claude H <hirschclaude@hotmail.fr>
2021-04-30 13:41:38 +0200

    Merge game.js and game2.js, make a object

--- src/game.js
+++ src/game.js
@@ -1,57 +1,112 @@
-var fs = require('fs');
-// file is included here:
-eval(fs.readFileSync('game2.js')+'');
-const util = require('util');
-const readline = require('readline');
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
+class TicTacToe {
 
-const question = util.promisify(rl.question).bind(rl);
+    util = require('util');
+    readline = require('readline');
+    rl = this.readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+    });
+    question = this.util.promisify(this.rl.question).bind(this.rl);
 
-async function startGame(){
-    let finish = false;
-    let nb_turn = 1;
-    displayBoard();
-    do{
-        let rep = "";
-        let playerMark = (nb_turn%2>0) ? "X" : "O";
-        let questionToDisplay = setQuestion(nb_turn);
-        try{
-            rep = await question( `${questionToDisplay} ? `);
-            // console.log("reponse : ",rep);
-        }catch(err){
-            // console.log("error : ",err);
-            rep = err;
+    async startGame(){
+        let finish = false;
+        let nb_turn = 1;
+        this.displayBoard();
+        do{
+            let rep = "";
+            let playerMark = (nb_turn%2>0) ? "X" : "O";
+            let questionToDisplay = this.setQuestion(nb_turn);
+            try{
+                rep = await this.question( `${questionToDisplay} ? `);
+                // console.log("reponse : ",rep);
+            }catch(err){
+                // console.log("error : ",err);
+                rep = err;
+            }
+            if(this.isCorrect(rep) && !this.isPlayed(rep)){
+                this.changeBoardValue(rep,playerMark)
+                nb_turn++;
+                if(nb_turn >= 5 && this.isVictory(parseInt(rep))){
+                    finish = true;
+                }
+            }
+        }while(!finish);
+        let joueurNumber = (nb_turn%2>0) ? "Joueur 2" : "Joueur 1";
+        console.log(`Victoire du ${joueurNumber}, gg ez`);
+    }
+
+    isCorrect(rep){
+        if(rep > 0 && rep < 10){
+            return true;
+        }else{
+            return false;
         }
-        if(isCorrect(rep) && !isPlayed(rep)){
-            changeBoardValue(rep,playerMark)
-            nb_turn++;
-            if(nb_turn >= 5 && isVictory(parseInt(rep))){
-                finish = true;
+    }
+
+    setQuestion(nbTurn){
+        if( nbTurn%2>0 ){ //Impair 1er joueur
+            return "Joueur 1 (X) : Votre choix";
+        }else{ //Pair 2eme joueur
+            return "Joueur 2 (O) : Votre choix";
+        }
+    }
+
+    /* ---------------Game 2.js-------------------- */
+    board = {
+        1: ' 1 ',
+        2: ' 2 ',
+        3: ' 3 ',
+        4: ' 4 ',
+        5: ' 5 ',
+        6: ' 6 ',
+        7: ' 7 ',
+        8: ' 8 ',
+        9: ' 9 ',
+    };
+    
+    winCombinaison = [
+        [1,2,3],
+        [4,5,6],
+        [7,8,9],
+        [1,5,9],
+        [3,5,7],
+        [1,4,7],
+        [2,5,8],
+        [3,6,9],
+    ]
+    
+    
+    
+    displayBoard() {
+        console.log('\n' +
+        this.board[1] + '|' + this.board[2] + '|' + this.board[3] + '\n' +
+        this.board[4] + '|' + this.board[5] + '|' + this.board[6] + '\n' +
+        this.board[7] + '|' + this.board[8] + '|' + this.board[9] + '\n'
+        );
+    }
+    
+    changeBoardValue(index, sign) {
+        this.board[index] = ' ' + sign + ' ';
+        this.displayBoard();
+    }
+    
+    isPlayed(index){
+        return this.board[index] === ' X ' || this.board[index] === ' O ';
+    }
+    
+    isVictory(index) {
+        let victory = false
+        this.winCombinaison.forEach((value) => {
+        if (value.includes(index)) {
+            if (this.board[value[0]] === this.board[value[1]] && this.board[value[1]] === this.board[value[2]]) {
+            victory = true;
             }
         }
-    }while(!finish);
-    let joueurNumber = (nb_turn%2>0) ? "Joueur 2" : "Joueur 1";
-    console.log(`Victoire du ${joueurNumber}, gg ez`);
-}
+        })
+        return victory;
+    };
 
-function isCorrect(rep){
-    if(rep > 0 && rep < 10){
-        return true;
-    }else{
-        return false;
-    }
 }
-
-function setQuestion(nbTurn){
-    if( nbTurn%2>0 ){ //Impair 1er joueur
-        return "Joueur 1 (X) : Votre choix";
-    }else{ //Pair 2eme joueur
-        return "Joueur 2 (O) : Votre choix";
-    }
-}
-
-startGame();
+let game = new TicTacToe();
+game.startGame();
 
